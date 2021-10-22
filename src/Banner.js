f08a0e2863c0b1d0e7f06069a1476e0e32c18d60
Shashwat Singh Raghav <54545252+ShashwatSinghRaghav@users.noreply.github.com>
2021-10-22 15:15:25 +0530

    banner done

--- src/Banner.js
+++ src/Banner.js
@@ -11,7 +11,7 @@ function Banner() {
         <div className = "banner">
             <div className='banner__search'>
                 {showSearch && <Search />}
-                <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>Search Dates</Button>
+                <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>{showSearch ? "Hide" : "Search Date"}</Button>
             </div>
             <div className='banner__info'>
                 <h1>Get out and stretch your imagination</h1>
--- src/Search.js
+++ src/Search.js
@@ -3,6 +3,8 @@ import './Search.css'
 import { DateRangePicker } from 'react-date-range';
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css"
+import { People } from '@material-ui/icons';
+import { Button } from '@material-ui/core';
 
 function Search() {
     const [startDate, setStartDate] = useState(new Date());
@@ -22,6 +24,11 @@ function Search() {
     return (
         <div className='search'>
             <DateRangePicker ranges={[selectionRange]} onChange={handleSelect}/>
+            <h2>
+                Number of guests <People/>
+            </h2>
+            <input min={0} defaultValue={2} type="number"/>
+            <Button>Search Airbnb</Button>
         </div>
     )
 }
